7cf46476e3f109ba7bb233591faf1651fa95c1ff
fzanollo <fzanollo@dc.uba.ar>
2023-05-15 01:37:53 -0300

    renames

--- com4ppl.py
+++ com4ppl.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import textdistance
 from numpy import median
+from pandas import DataFrame
 from thefuzz import fuzz
 
 # Valid algorithms
@@ -38,42 +39,42 @@ comparisonAlgorithms = {
 config = {}
 
 
-def loadConfig(data):
+def load_config(data):
     # files
     config['configFilesPath'] = data['configFilesPath']
 
     # compatible rows config
-    variableFields = pd.read_csv(f"{data['configFilesPath']}/{data['variableFields']}").set_index('variable')
-    compatibleData = pd.read_csv(f"{data['configFilesPath']}/{data['compatibleData']}").set_index('variable')
-    compatibleData['equivalences'] = compatibleData[compatibleData.type == 'categorical'].parameter.apply(
-        loadEquivalences)
-    config['compatibleSettings'] = compatibleData.join(variableFields).to_dict(orient='index')
+    variable_fields = pd.read_csv(f"{data['configFilesPath']}/{data['variableFields']}").set_index('variable')
+    compatible_data = pd.read_csv(f"{data['configFilesPath']}/{data['compatibleData']}").set_index('variable')
+    compatible_data['equivalences'] = compatible_data[compatible_data.type == 'categorical'].parameter.apply(
+        load_equivalences)
+    config['compatibleSettings'] = compatible_data.join(variable_fields).to_dict(orient='index')
 
     # schemes
     config['schemesToUse'] = pd.read_csv(f"{data['configFilesPath']}/{data['schemesToUse']}", names=['schemes'])[
         'schemes'].to_list()
     # TODO add option for "fast scheme"
-    schemesConfig = pd.read_csv(f"{data['configFilesPath']}/{data['schemesConfig']}")
-    schemesConfig.rename(columns={'listA.column': 'key1', 'listB.column': 'key2'}, inplace=True)
-    if schemesConfig.dtypes['threshold'] == str:
-        schemesConfig.threshold = schemesConfig.threshold.str.replace(',', '.').astype(float)
-    config['schemesConfig'] = schemesConfig
+    schemes_config = pd.read_csv(f"{data['configFilesPath']}/{data['schemesConfig']}")
+    schemes_config.rename(columns={'listA.column': 'key1', 'listB.column': 'key2'}, inplace=True)
+    if schemes_config.dtypes['threshold'] == str:
+        schemes_config.threshold = schemes_config.threshold.str.replace(',', '.').astype(float)
+    config['schemesConfig'] = schemes_config
 
     # algorithms
-    algorithmsConfig = pd.read_csv(f"{data['configFilesPath']}/{data['algorithmsConfig']}")
-    algorithmsConfig['function'] = algorithmsConfig.scores.map(comparisonAlgorithms)
-    config['algorithmsConfig'] = algorithmsConfig
+    algorithms_config = pd.read_csv(f"{data['configFilesPath']}/{data['algorithmsConfig']}")
+    algorithms_config['function'] = algorithms_config.scores.map(comparisonAlgorithms)
+    config['algorithmsConfig'] = algorithms_config
 
 
-def loadEquivalences(filename):
-    if not isNaN(filename):
-        equivalencesDF = pd.read_csv(f"{config['configFilesPath']}/{filename}")
-        return list(equivalencesDF.itertuples(index=False, name=None))
+def load_equivalences(filename):
+    if not is_Nan(filename):
+        equivalences_df = pd.read_csv(f"{config['configFilesPath']}/{filename}")
+        return list(equivalences_df.itertuples(index=False, name=None))
     else:
         return np.nan
 
 
-def isNaN(value):
+def is_Nan(value):
     if type(value) == str:
         return value.upper() == 'NAN'
     elif type(value) == list:
@@ -82,7 +83,7 @@ def isNaN(value):
         return np.isnan(value)
 
 
-def findCompatibleRows(row1, db2):
+def find_compatible_rows(row1, db2):
     compatibles = db2
 
     for variable, options in config['compatibleSettings'].items():
@@ -95,7 +96,7 @@ def findCompatibleRows(row1, db2):
 
                 # If the value on the current row is NaN and matching with NaN is allowed,
                 # we should not filter anything, since anything will match with it
-                if isNaN(row1[field]) and allowNa:
+                if is_Nan(row1[field]) and allowNa:
                     continue
 
                 filtered = pd.DataFrame()
@@ -111,7 +112,7 @@ def findCompatibleRows(row1, db2):
                     equivalences = options['equivalences']
 
                     # TODO find another way to represent equivalences or "move" to preprocessing.
-                    if not isNaN(equivalences):
+                    if not is_Nan(equivalences):
                         for v1, v2 in equivalences:
                             equivalent = pd.DataFrame()
 
@@ -132,11 +133,11 @@ def findCompatibleRows(row1, db2):
     return compatibles.index
 
 
-def findCompatibles(base1, base2):
-    compatibles = base1.apply(lambda r1: findCompatibleRows(r1, base2), axis=1)
+def find_compatibles(base1: DataFrame, base2: DataFrame):
+    compatibles = base1.apply(lambda r1: find_compatible_rows(r1, base2), axis=1)
     # Convert compatibles index series to complete dataframe with row information
-    compatiblesList = []
-    for i1, c in compatibles.iteritems():
+    compatibles_list = []
+    for i1, c in compatibles.items():
         for i2 in c:
             row1 = base1.iloc[i1]
             row2 = base2.iloc[i2]
@@ -144,12 +145,12 @@ def findCompatibles(base1, base2):
             row1.set_axis(['base1_' + x for x in base1.columns], inplace=True)
             row2.set_axis(['base2_' + x for x in base2.columns], inplace=True)
 
-            compatiblesList.append(pd.concat([row1, row2]))
+            compatibles_list.append(pd.concat([row1, row2]))
 
-    return pd.DataFrame(compatiblesList)
+    return pd.DataFrame(compatibles_list)
 
 
-def findCandidates(compatiblesDF, verbose=False):
+def find_candidates(compatibles_df, verbose=False):
     for scheme, key1, key2, threshold in config['schemesConfig'].itertuples(index=False):
         key1 = 'base1_' + key1
         key2 = 'base2_' + key2
@@ -159,32 +160,32 @@ def findCandidates(compatiblesDF, verbose=False):
             continue
         if verbose:
             print(f"Running scheme {scheme}")
-        if key1 not in compatiblesDF.columns:
+        if key1 not in compatibles_df.columns:
             print(f"Error: Invalid key on database 1 {key1}, skipping")
             continue
-        if key2 not in compatiblesDF.columns:
+        if key2 not in compatibles_df.columns:
             print(f"Error: Invalid key on database 2 {key2}, skipping")
             continue
 
-        compatiblesDF[f"elems_median_{scheme}"] = 0
+        compatibles_df[f"elems_median_{scheme}"] = 0
 
         for algorithmName, doCalculate, addToMedian, function in config['algorithmsConfig'].itertuples(index=False):
             if 'yes' in doCalculate.lower():
-                value = compatiblesDF.apply(lambda x: round(function(x[key1], x[key2]), 4), axis=1)
-                compatiblesDF[f"{algorithmName}_{scheme}"] = value
+                value = compatibles_df.apply(lambda x: round(function(x[key1], x[key2]), 4), axis=1)
+                compatibles_df[f"{algorithmName}_{scheme}"] = value
 
                 if addToMedian:
-                    compatiblesDF[f"elems_median_{scheme}"] += value
+                    compatibles_df[f"elems_median_{scheme}"] += value
 
-        compatiblesDF[f"median_{scheme}"] = median(compatiblesDF[f"elems_median_{scheme}"])
-        compatiblesDF.drop(f"elems_median_{scheme}", axis=1, inplace=True)
-        compatiblesDF = compatiblesDF[compatiblesDF[f"median_{scheme}"] >= threshold]
+        compatibles_df[f"median_{scheme}"] = median(compatibles_df[f"elems_median_{scheme}"])
+        compatibles_df.drop(f"elems_median_{scheme}", axis=1, inplace=True)
+        compatibles_df = compatibles_df[compatibles_df[f"median_{scheme}"] >= threshold]
 
-    return sortCandidatesDF(compatiblesDF)
+    return sort_candidates_df(compatibles_df)
 
 
-def sortCandidatesDF(candidatesDF):
-    candidatesDF.to_csv(f"prueba.csv", index=False)
-    sortPriority = [f"median_{scheme}" for scheme in config['schemesToUse']]
-    return candidatesDF.sort_values(
-        sortPriority, ascending=False, kind='stable', ignore_index=True)
+def sort_candidates_df(candidates_df):
+    candidates_df.to_csv(f"prueba.csv", index=False)
+    sort_priority = [f"median_{scheme}" for scheme in config['schemesToUse']]
+    return candidates_df.sort_values(
+        sort_priority, ascending=False, kind='stable', ignore_index=True)
--- matching.py
+++ matching.py
@@ -16,45 +16,45 @@ def main(interactive, verbose):
 
     if interactive:
         # ask for database paths
-        base1Path = input("Please insert database 1 path (leave empty to use default):")
-        while base1Path != '' and not os.path.isfile(base1Path):
+        base1_path = input("Please insert database 1 path (leave empty to use default):")
+        while base1_path != '' and not os.path.isfile(base1_path):
             print("File does not exists")
-            base1Path = input("Please insert database 1 path (leave empty to use default):")
+            base1_path = input("Please insert database 1 path (leave empty to use default):")
 
-        base2Path = input("Please insert database 2 path (leave empty to use default):")
-        while base2Path != '' and not os.path.isfile(base2Path):
+        base2_path = input("Please insert database 2 path (leave empty to use default):")
+        while base2_path != '' and not os.path.isfile(base2_path):
             print("File does not exists")
-            base2Path = input("Please insert database 2 path (leave empty to use default):")
+            base2_path = input("Please insert database 2 path (leave empty to use default):")
 
-        if base1Path != '':
-            data['base1Path'] = base1Path
-        if base2Path != '':
-            data['base2Path'] = base2Path
+        if base1_path != '':
+            data['base1Path'] = base1_path
+        if base2_path != '':
+            data['base2Path'] = base2_path
 
         res = input(f"Do you want to change output folder (from {data['outputPath']})? (y/n):")
         if 'y' in res:
-            outputPath = input("Please insert new output path:")
+            output_path = input("Please insert new output path:")
             success = False
             while not success:
                 try:
-                    os.makedirs(outputPath, exist_ok=True)
+                    os.makedirs(output_path, exist_ok=True)
                 except OSError as e:
                     print(f"Failed to find/create output folder with error: {e}")
-                    outputPath = input("Please insert new output path:")
+                    output_path = input("Please insert new output path:")
                 else:
                     success = True
-            data['outputPath'] = outputPath
+            data['outputPath'] = output_path
 
-        outputFileName = input("Please insert output file name (leave empty to use default):")
-        if outputFileName != '':
-            data['outputFileName'] = outputFileName
+        output_file_name = input("Please insert output file name (leave empty to use default):")
+        if output_file_name != '':
+            data['outputFileName'] = output_file_name
 
-    com4ppl.loadConfig(data)
-    makeMatching(data, verbose)
+    com4ppl.load_config(data)
+    make_matching(data, verbose)
 
 
-def makeMatching(data, verbose):
-    maxUsedMem = 0
+def make_matching(data, verbose):
+    max_used_mem = 0
     # output folder
     os.makedirs(data['outputPath'], exist_ok=True)
 
@@ -66,34 +66,34 @@ def makeMatching(data, verbose):
     print(f"\tdb2: {data['base2Path']}, size: {base2.shape[0]}")
 
     # TODO check if this way of getting the used mem is correct
-    maxUsedMem = max(maxUsedMem, round(python_process.memory_info().rss / 1e+9, 4))
+    max_used_mem = max(max_used_mem, round(python_process.memory_info().rss / 1e+9, 4))
 
     # find compatible rows ------------------------------------------------------------- step 1
     print("Finding compatible pairs")
     sub_timer_start = time.time()
 
-    compatiblesDF = com4ppl.findCompatibles(base1, base2)
+    compatibles_df = com4ppl.find_compatibles(base1, base2)
 
-    print(f"\tFound {compatiblesDF.shape[0]} compatible rows in {round(time.time() - sub_timer_start, 4)} seconds")
-    maxUsedMem = max(maxUsedMem, round(python_process.memory_info().rss / 1e+9, 4))
+    print(f"\tFound {compatibles_df.shape[0]} compatible rows in {round(time.time() - sub_timer_start, 4)} seconds")
+    max_used_mem = max(max_used_mem, round(python_process.memory_info().rss / 1e+9, 4))
 
     # run configured schemes on compatible pairs  -------------------------------------- step 2
     print("Running schemes on compatible pairs")
     sub_timer_start = time.time()
 
-    candidatesDF = com4ppl.findCandidates(compatiblesDF, verbose)
+    candidates_df = com4ppl.find_candidates(compatibles_df, verbose)
 
-    print(f"\tFound {candidatesDF.shape[0]} candidates in {round(time.time() - sub_timer_start, 4)} seconds")
-    maxUsedMem = max(maxUsedMem, round(python_process.memory_info().rss / 1e+9, 4))
+    print(f"\tFound {candidates_df.shape[0]} candidates in {round(time.time() - sub_timer_start, 4)} seconds")
+    max_used_mem = max(max_used_mem, round(python_process.memory_info().rss / 1e+9, 4))
 
     # output result -------------------------------------------------------------------- step 3
-    if candidatesDF.shape[0] > 0:
-        candidatesDF.to_csv(f"{data['outputPath']}/{data['outputFileName']}.csv", index=False)
+    if candidates_df.shape[0] > 0:
+        candidates_df.to_csv(f"{data['outputPath']}/{data['outputFileName']}.csv", index=False)
         print(f"Finished sorting candidates, results are in {data['outputPath']}")
     else:
         print("No candidates found")
 
-    print(f"Maximum used memory: {maxUsedMem} GB")
+    print(f"Maximum used memory: {max_used_mem} GB")
 
 
 if __name__ == '__main__':
